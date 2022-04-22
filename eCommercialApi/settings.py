54b0e21c48f5d59fa47262eedcfdf9d0dd8eb460
Long Phu <phutl@icd-vn.com>
2022-04-22 16:51:38 +0700

    Update settings.py

--- eCommercialApi/settings.py
+++ eCommercialApi/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = 'django-insecure-99gc0c4qwe58$@ebvn=zd8&^tx*7u@bo&k@_&lou*a*00)vwgb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
